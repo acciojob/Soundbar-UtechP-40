14d76c9a5fc79930b72e7465698b6d0183fe5620
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-26 14:35:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+//your JS code here. If required.
+ let btn = document.querySelectorAll(".btn")
+ 
+ let stop = document.querySelector(".stop")
+ for(let t of btn){
+    t.addEventListener("click",(e)=>{
+        let my_audio = new Audio(`./sounds/${t.innerText}.mp3`)
+        // let my_audio = "./sounds"
+        my_audio.play();
+ stop.addEventListener("click",(e)=>{
+    my_audio.pause();
+ })
+    })
+ }
+ 
